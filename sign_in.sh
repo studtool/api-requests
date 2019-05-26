b52eb87e30606bb720d82114e5eb823ea67d9fcc
Denis Zinovyev <zinoviev.denis@yandex.ru>
2019-05-26 03:12:34 +0300

    signIn()

---
+++ sign_in.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+address="${1}"
+email="${2}"
+password="${3}"
+
+curl -X POST -d "{\"email\":\"${email}\",\"password\":\"${password}\"}" \
+    -H "Content-Type: application/json" "http://${address}/api/v0/public/auth/sessions" -v
